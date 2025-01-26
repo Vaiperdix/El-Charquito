33fdd88f6288cb9230d2113ea46036d9fa81c1de
SamSam <168691193+samtesito@users.noreply.github.com>
2025-01-26 15:01:40 -0400

    Camara arreglada, soundtrack listo

--- El Charquito/Assets/CameraControl.cs
+++ El Charquito/Assets/CameraControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float FollowSpeed = 6f;
+    public float FollowSpeed = 4f;
     public float yOffset =1f;
     public Transform target;
 
